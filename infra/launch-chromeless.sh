1791a91c0eb1bee8b28ae494dbe8864d9e5672e6
Iggy <iggy.gullstrand@gmail.com>
2026-05-07 05:36:03 +0200

    fix(runtime): bind devtools behind proxy

--- infra/launch-chromeless.sh
+++ infra/launch-chromeless.sh
@@ -134,7 +134,7 @@ exec "${CHROMELESS_BROWSER_BIN}" \
   --disable-dev-shm-usage \
   --display=:99 \
   --remote-debugging-port=9222 \
-  --remote-debugging-address=0.0.0.0 \
+  --remote-debugging-address=127.0.0.1 \
   --remote-allow-origins=* \
   --user-data-dir=/home/cbuser/.config/chromium \
   --no-first-run \
